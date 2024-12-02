5fc4bab67f77aa99cd0ad0a794739b6f353686b2
VinitKarlekar <vinitkarlekar84474@gmail.com>
2024-12-02 13:00:57 +0530

    Further changes on summary

--- SumaryOfMachine LearningOnramp.m
+++ SumaryOfMachine LearningOnramp.m
@@ -1,5 +1,5 @@
 
-Import and Process Data
+##Import and Process Data
 
 The readtable function creates a table in MATLAB from a data file.	
 allStats = readtable("bballStats.txt");
@@ -22,3 +22,38 @@ playerStats = groupsummary(allStats,"playerID","sum");
 
 The innerjoin function merges two tables, retaining only the common key variable observations.	
 data = innerjoin(playerInfo,playerStats);
+
+##Visualize and Engineer Features
+
+The boxplot function can create separate box plots based on a grouping variable.	
+boxplot(data.height,data.pos)
+
+
+You can use indexing and element-wise division to scale variables in a table.	
+data{:,8:22} = data{:,8:22}./data.minutes;
+data.minutes = data.minutes./data.GP;
+
+
+You can use the gscatter function to create a grouped scatter plot.	
+gscatter(data.rebounds,data.points,data.pos)
+
+
+
+##Train and Evaluate a Model
+The fitcknn function fits a k-nearest neighbors classification model.	
+knnmodel = fitcknn(data,"pos");
+
+You can use property name-value pairs to modify model options.	
+knnmodel = fitcknn(data,"pos","NumNeighbors",5,...
+      "Standardize",true);
+
+You can calculate the misclassification rate for a data set using the loss function.	
+mdlLoss = loss(knnmodel,dataTest)
+mdlLoss =
+    0.4085
+
+The predict function uses a classification model to predict classes for observations.	
+predPos = predict(knnmodel,dataTest);
+
+You can use the confusionchart function to visually compare true classes and predicted classes.	
+confusionchart(data.pos,predPos);
